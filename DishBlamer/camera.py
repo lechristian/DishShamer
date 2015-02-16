41d8571162d3635853d8a0115892faab0e33d1d2
schasins <schasins@gmail.com>
2015-02-16 03:44:26 -0800

    Adding facial recognition, so we know who's actually adding and removing plates.  Adding a leaderboard, accessible via a web interface.  Adding emails.  Can send out emails with a little leaderboard at any time, and it chides the worst washer, congratulates the best.

--- DishBlamer/camera.py
+++ DishBlamer/camera.py
@@ -1,10 +1,126 @@
 import cv2
 import time
-import serial
+
+#import serial
 from datetime import datetime
+
+# Import smtplib for the actual sending function
+import smtplib
+
+# Import the email modules we'll need
+from email.mime.text import MIMEText
+
+import operator
+import json
 
 # Serial port for Arduino
 SERIAL_PORT = "/dev/tty.usbmodem1411"
+
+testing = True
+
+log = open("log.txt","r+w")
+logContent = log.read()
+logArray = json.loads(logContent)
+emails = {"Sarah":"schasins@gmail.com","Alison":"schasins@gmail.com","Baxter":"schasins@gmail.com","Christian":"schasins@gmail.com"}
+GMAIL_USER = 'yourangrydishes@gmail.com'
+GMAIL_PASS = 'yosupyosup'
+histograms = {}
+
+def features(imageFile):
+    img=cv2.imread(imageFile)
+    hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    histogram = cv2.calcHist([hsv], [0, 1], None, [180, 256], [0, 180, 0, 256])
+    Nhistogram=cv2.normalize(histogram)
+    return Nhistogram.flatten()
+
+def setupComparisonHistograms():
+    for name in emails:
+        histogram = features("images/training/"+name+".jpg")
+        histograms[name] = histogram
+    return
+
+def guessUser(filename):
+    currHist = features(filename)
+    bestSimSoFar = -100000000000000000000
+    nameGuessSoFar = ""
+    for name in histograms:
+        sim = cv2.compareHist(currHist,histograms[name],cv2.cv.CV_COMP_CORREL)
+        if sim > bestSimSoFar:
+            bestSimSoFar = sim
+            nameGuessSoFar = name
+    return nameGuessSoFar
+
+def processNewEvent(filename, addedDishes):
+    global log
+    timestamp = time.time()
+    name = guessUser(filename)
+    scoreDiff = 1
+    if not addedDishes:
+        scoreDiff = -1
+    logArray.append([name,timestamp,scoreDiff,filename])
+    log.truncate(0)
+    log.seek(0)
+    log.write(json.dumps(logArray))
+    log.close()
+    log = open("log.txt","r+w")
+
+def generateEmail():
+    scores = {}
+    for cells in logArray:
+        name = cells[0]
+        scores[name] = scores.get(name,0)+int(cells[2]) # -1 if removed plates, +1 if added plates
+    sortedScores = sorted(scores.items(), key=operator.itemgetter(1), reverse=True)
+    print sortedScores
+
+    emailString = ""
+    best = None
+    worst = None
+    for i in range(len(sortedScores)):
+        score = sortedScores[i]
+        emailString += score[0]+":\t"+str(score[1])+"\n"
+        if i == 0:
+            best = score[0]
+        if i == (len(sortedScores)-1):
+            worst = score[0]
+
+    for name in emails:
+        addition = "Your dish washing results, "+name
+        if name == best:
+            addition = "You're the best, "+name+"!!!!!"
+        elif name == worst:
+            addition = "You're in trouble, "+name+"!!!!!"
+        currEmailString = addition+"\n"+emailString
+
+        recipient = emails[name]
+
+        smtpserver = smtplib.SMTP("smtp.gmail.com",587)
+        smtpserver.ehlo()
+        smtpserver.starttls()
+        smtpserver.ehlo
+        smtpserver.login(GMAIL_USER, GMAIL_PASS)
+        header = 'To:' + recipient + '\n' + 'From: ' + GMAIL_USER
+        header = header + '\n' + 'Subject:' + addition + '\n'
+        print header
+        msg = header + '\n' + currEmailString + ' \n\n'
+        smtpserver.sendmail(GMAIL_USER, recipient, msg)
+        smtpserver.close()
+
+def testWithoutSerialInput():
+    time.sleep(5);
+    processNewEvent("images/a.jpg",True)
+    time.sleep(6);
+    processNewEvent("images/g.jpg",False)
+    time.sleep(5);
+    processNewEvent("images/c.jpg",True)
+    time.sleep(6);
+    processNewEvent("images/f.jpg",False)
+    time.sleep(3);
+    processNewEvent("images/b.jpg",False)
+    time.sleep(4);
+    processNewEvent("images/h.jpg",True)
+    time.sleep(3)
+    processNewEvent("images/e.jpg",True)
+
 
 def imageCapture():   
     cap = cv2.VideoCapture(0)
@@ -16,19 +132,33 @@ def imageCapture():
         try:
             data = s.read(s.inWaiting())
             if data:
-                if data == "c":
+                curDateTime = ""
+                if data == "g" or data == "b":
                     ret, frame = cap.read()
                     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
 
                     curDateTime = datetime.now().strftime('%m.%d.%Y-%H.%M.%s')
                     curDateTime += '.jpg'
                     out = cv2.imwrite(curDateTime, frame)
+                if data == "g":
+                    #plates returned
+                    processNewEvent(curDateTime,true)
+                elif data == "b":
+                    #plates removed
+                    processNewEvent(curDateTime,false)
+
 
         except serial.serialutil.SerialException:
             pass
 
 if __name__ == "__main__":
+    #generateEmail()
+    setupComparisonHistograms()
     try:
-        imageCapture()
+        if testing:
+            testWithoutSerialInput()
+            generateEmail()
+        else:
+            imageCapture()
     except KeyboardInterrupt:
         pass
---
+++ DishBlamer/update.js
@@ -0,0 +1,121 @@
+var oldData = null;
+
+var update = function(){
+	$.ajax({
+	  dataType: "json",
+	  url: "log.txt",
+	  success: function(data){
+
+	  	if (oldData!== null && data.length === oldData.length){
+	  		return;
+	  	}
+
+	  	var thisRoundName = null;
+	  	var thisRoundPositive = null;
+	  	if (oldData !== null){
+	  		for (var i = oldData.length; i<data.length; i++){
+	  			//this represents a new item
+	  			var name = data[i][0];
+	  			thisRoundName = name;
+	  			var color = "#EBFFD6";
+	  			thisRoundPositive = true;
+	  			if (parseInt(data[i][2]) === -1){
+	  				color = "#FFD6DB";
+	  				thisRoundPositive = false;
+	  			}
+	  			$("#notification").html("<div id='notification_inner' style='background-color:"+color+"'><div>"+name+"</div><img style='width:500px' src='"+data[i][3]+"'></div>");
+	  			$("#notification").fadeTo(500, 1.0);
+	  		}
+	  	}
+
+	  	var scores = {};
+	  	for (var i = 0; i<data.length; i++){
+	  		var cells = data[i];
+	  		var name = cells[0];
+	  		scoreSoFar = scores[name];
+	  		if (!scoreSoFar){ scoreSoFar = 0;}
+	  		scoreSoFar+=parseInt(cells[2]);
+	  		scores[name] = scoreSoFar;
+	  	}
+			var sortable = [];
+			for (var name in scores)
+			      sortable.push([name, scores[name]]);
+			sortable.sort(function(a, b) {return a[1] - b[1]}).reverse();
+
+	  	var tableStr = "<table style='width:400px'>";
+	  	tableStr += "<thead><tr><th>User</th><th>Returns Minus Checkouts</th></tr></thead><tbody>";
+	  	for (var i = 0; i<sortable.length; i++){
+	  		var name = sortable[i][0];
+	  		var score = sortable[i][1];
+	  		var extra = "";
+	  		if (name === thisRoundName){
+	  			extra = "class='"+thisRoundPositive+"'";
+	  		}
+	  		tableStr += "<tr "+extra+"><td>"+name+"</td><td>"+score+"</td></tr>";
+	  	}
+	  	tableStr += "</tbody></table>";
+	  	$("#returns").html(tableStr);
+
+	  	var timeSums = {};
+	  	var numCheckoutReturnPairs = {}
+	  	for (var i = 0; i<data.length; i++){
+	  		var cells = data[i];
+	  		if (parseInt(cells[2]) === 1){
+	  			continue;
+	  		}
+	  		//we have a checkout!
+	  		var name = cells[0];
+	  		timeSumSoFar = timeSums[name];
+	  		if (!timeSumSoFar){ timeSumSoFar = 0;}
+	  		numCheckoutsSoFar = numCheckoutReturnPairs[name];
+	  		if (!numCheckoutsSoFar){ numCheckoutsSoFar = 0;}
+
+	  		for (var j = i; j<data.length; j++){
+	  			var newCells = data[j];
+	  			if (newCells[0] === name){
+	  				if (newCells[2] === 1){
+	  					var timeDiff = parseFloat(newCells[1])-parseFloat(cells[1]);
+	  					timeSumSoFar += timeDiff;
+	  					timeSums[name] = timeSumSoFar;
+	  					numCheckoutReturnPairs[name] = numCheckoutsSoFar+1;
+	  					break;
+	  				}
+	  			}
+	  		}
+	  	}
+			sortable = [];
+			for (var name in timeSums)
+			      sortable.push([name, timeSums[name]/numCheckoutReturnPairs[name]]);
+			sortable.sort(function(a, b) {return a[1] - b[1]});
+
+	  	var tableStr = "<table style='width:400px'>";
+	  	tableStr += "<tr><th>User</th><th>Average Return Time</th></tr>";
+	  	for (var i = 0; i<sortable.length; i++){
+	  		var name = sortable[i][0];
+	  		var score = sortable[i][1];
+	  		extra = "";
+	  		if (name === thisRoundName){
+	  			extra = "class='"+thisRoundPositive+"'";
+	  		}
+	  		tableStr += "<tr "+extra+"><td>"+name+"</td><td>"+score+"</td></tr>";
+	  	}
+	  	tableStr += "</tbody></table>";
+	  	$("#times").html(tableStr);
+
+	  	oldData = data;
+
+	  	setTimeout(removeHighlights,1000);
+	  }
+	});
+
+	setTimeout(update,500);
+};
+$(update);
+
+var removeHighlights = function(){
+	$("#notification").fadeTo(2000, 0.0);
+	setTimeout(function(){
+		$("tr").removeClass("false");
+		$("tr").removeClass("true");
+	},1700);
+};
